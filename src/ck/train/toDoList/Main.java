a26059d078f4c962b685a83eafd4253e20f35039
Chris <christian.kranz@gmail.com>
2019-12-18 11:47:21 +0100

    basic settings

--- src/ck/train/toDoList/Main.java
+++ src/ck/train/toDoList/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("ToDo List");
+        primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
     }
 
